a318cc01dbffbe299a0c8453ba20bd24749158b4
ahwanfebriantoro <ahwanfebrian01@gmail.com>
2021-03-17 01:45:10 +0700

    fix: input prop type

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,12 @@
+import { DetailedHTMLProps, InputHTMLAttributes } from 'react';
 import cssInput from './Input.module.scss';
 
-const Input = (props) => {
+const Input = (
+  props: DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  >
+) => {
   return (
     <input className={cssInput.input} placeholder="placeholder" {...props} />
   );
